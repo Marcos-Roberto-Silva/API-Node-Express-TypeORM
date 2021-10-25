65dd47bba0c321b9928173fd312bde60d56bd41e
Marco Silva <marcos.1998226@gmail.com>
2021-10-25 16:30:24 -0300

    Generated compliments table

--- src/database/migrations/1635186794160-CreateCompliments.ts
+++ src/database/migrations/1635186794160-CreateCompliments.ts
@@ -1,11 +1,70 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateCompliments1635186794160 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
+    public async up(queryRunner : QueryRunner): Promise < void > {
+        await queryRunner.createTable(new Table({
+            name: "compliments",
+            columns: [
+                {
+                    name: "id",
+                    type: "uuid",
+                    isPrimary: true
+                },
+                {
+                    name: "user_sender",
+                    type: "uuid"
+                },
+                {
+                    name: "user_receiver",
+                    type: "uuid"
+                },
+                {
+                    name: "tag_id",
+                    type: "uuid"
+                }, {
+                    name: "message",
+                    type: "varchar"
+                }, {
+                    name: "created_at",
+                    type: "timestamp",
+                    default: "now()"
+                }, {
+                    name: "updated_at",
+                    type: "timestamp",
+                    default: "now()"
+                }
+            ],
+            foreignKeys: [
+                {
+                    name: "FKUserSenderCompliments",
+                    referencedTableName: "Users",
+                    referencedColumnNames: ["id"],
+                    columnNames: ["user_sender"],
+                    onDelete: "SET NULL",
+                    onUpdate: "SET NULL"
+                }, {
+                    name: "FKUserReceiverCompliments",
+                    referencedTableName: "Users",
+                    referencedColumnNames: ["id"],
+                    columnNames: ["user_sender"],
+                    onDelete: "SET NULL",
+                    onUpdate: "SET NULL"
+                }, {
+                    name: "FKTagCompliments",
+                    referencedTableName: "tags",
+                    referencedColumnNames: ["id"],
+                    columnNames: ["tag_id"],
+                    onDelete: "SET NULL",
+                    onUpdate: "SET NULL"
+
+                }
+            ]
+        }))
     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
+    public async down(queryRunner : QueryRunner): Promise < void > {
+        await queryRunner.dropDatabase("compliments");
     }
 
 }
